04eb294a3f19ae7250d0ec66b70be82884f9f6fc
kon72 <kinsei0916@gmail.com>
2024-05-02 04:10:01 +0900

    Don't fail when pip is not installed

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import os
 import sys
 from distutils.version import LooseVersion
 
-import pip
 from setuptools import find_packages, setup
 
 if LooseVersion(sys.version) < LooseVersion("3.6"):
@@ -13,11 +12,16 @@ if LooseVersion(sys.version) < LooseVersion("3.6"):
         "but your Python version is {}".format(sys.version)
     )
 
-if LooseVersion(pip.__version__) < LooseVersion("19"):
-    raise RuntimeError(
-        "pip>=19.0.0 is required, but your pip version is {}. "
-        'Try again after "pip install -U pip"'.format(pip.__version__)
-    )
+try:
+    import pip
+except ModuleNotFoundError:
+    pass
+else:
+    if LooseVersion(pip.__version__) < LooseVersion("19"):
+        raise RuntimeError(
+            "pip>=19.0.0 is required, but your pip version is {}. "
+            'Try again after "pip install -U pip"'.format(pip.__version__)
+        )
 
 # TODO(@dathudeptrai) update requirement if needed.
 requirements = {
